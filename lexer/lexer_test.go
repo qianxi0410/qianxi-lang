61df623d868e2042c4abb828a6f3bc8b33055fa3
qianxi0410 <894871277@qq.com>
2021-08-10 00:31:32 +0800

    Add lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,31 @@
+package lexer
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/monkey-lang/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `
+	!
+	let five = 5;
+	let ten = 10;
+	let add = fn(x, y) { x + y;
+	};
+	if (5 < 10) { return true;
+	} else { return false;
+	}
+	10 == 10; 10 != 9;
+		`
+
+	l := New(input)
+	for {
+		tok := l.NextToken()
+		if tok.Type == token.EOF {
+			return
+		}
+		fmt.Println(tok)
+	}
+}
