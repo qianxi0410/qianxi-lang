c364fa25269db673f2a7ee971ed7928086e03d48
qianxi0410 <894871277@qq.com>
2021-08-10 16:50:38 +0800

    Add repl

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/monkey-lang/repl"
+)
 
 func main() {
-	fmt.Println("hello world")
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!", user.Username)
+
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
