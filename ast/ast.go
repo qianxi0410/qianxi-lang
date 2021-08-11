0edc12e51867c8cd5dfd9bf5ceda2a6ee0d8a5fe
qianxi0410 <894871277@qq.com>
2021-08-11 12:11:43 +0800

    Fix typo

--- ast/ast.go
+++ ast/ast.go
@@ -17,12 +17,12 @@ type Expression interface {
 }
 
 type Program struct {
-	Statementes []Statement
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
-	if len(p.Statementes) > 0 {
-		return p.Statementes[0].TokenLiteral()
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
