3a57b0109f5aaddad36e21b7971c4842dfe66423
qianxi0410 <894871277@qq.com>
2021-08-11 11:17:21 +0800

    Add ast

---
+++ ast/ast.go
@@ -0,0 +1,50 @@
+package ast
+
+import "github.com/monkey-lang/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statementes []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statementes) > 0 {
+		return p.Statementes[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
