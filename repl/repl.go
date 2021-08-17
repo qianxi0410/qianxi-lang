f971fe35a2d5f7debb04816625f549b6949429a7
qianxi0410 <894871277@qq.com>
2021-08-17 10:26:39 +0800

    Update repl for evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/monkey-lang/evaluator"
 	"github.com/monkey-lang/lexer"
 	"github.com/monkey-lang/parser"
 )
@@ -31,9 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
 
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
