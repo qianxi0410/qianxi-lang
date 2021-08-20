362c51efdc3b1933e202dbd991721b695bb424cd
qianxi0410 <894871277@qq.com>
2021-08-20 20:12:34 +0800

    Add array builtins functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -341,19 +341,3 @@ func isError(obj object.Object) bool {
 	}
 	return false
 }
-
-var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("argument to `len` not supported, got %s", args[0].Type())
-			}
-		},
-	},
-}
