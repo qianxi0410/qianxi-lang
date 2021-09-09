48357ae17168849549d9fff2d079fe1adcdfcee5
qianxi0410 <894871277@qq.com>
2021-09-09 21:14:51 +0800

    Split the file of object

---
+++ object/type.go
@@ -0,0 +1,14 @@
+package object
+
+const (
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ         = "HASH"
+)
