d5e8c69f2c29d1db049b3060ef395d0dd033cac4
qianxi0410 <894871277@qq.com>
2021-09-14 19:55:04 +0800

    Add ast of float

---
+++ ast/float.go
@@ -0,0 +1,13 @@
+package ast
+
+import "github.com/monkey-lang/token"
+
+type FloatLiteral struct {
+	// token.FLOAT
+	Token token.Token
+	Value float64
+}
+
+func (fl *FloatLiteral) expressionNode()      {}
+func (fl *FloatLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FloatLiteral) String() string       { return fl.Token.Literal }
