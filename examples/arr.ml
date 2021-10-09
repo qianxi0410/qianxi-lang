a420a1ef458fbe975696b92d8ca98b0dad7c5e4c
qianxi0410 <894871277@qq.com>
2021-10-09 20:09:26 +0800

    Make go lint happy

--- examples/arr.ml
+++
@@ -1,9 +0,0 @@
-let rev = fn(arr) {
-    if len(arr) == 1 {
-        return arr;
-    } else {
-        return push([last(arr)], rev(rest(arr)));
-    }
-};
-
-rev([1, 2, 3]);
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -481,7 +481,7 @@ func TestParsingArrayLiterals(t *testing.T) {
 	p := New(l)
 	program := p.ParseProgram()
 	checkParserErrors(t, p)
-	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	stmt, _ := program.Statements[0].(*ast.ExpressionStatement)
 	array, ok := stmt.Expression.(*ast.ArrayLiteral)
 	if !ok {
 		t.Fatalf("exp not ast.ArrayLiteral. got=%T", stmt.Expression)
@@ -500,7 +500,7 @@ func TestParsingIndexExpressions(t *testing.T) {
 	p := New(l)
 	program := p.ParseProgram()
 	checkParserErrors(t, p)
-	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	stmt, _ := program.Statements[0].(*ast.ExpressionStatement)
 	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
 	if !ok {
 		t.Fatalf("exp not *ast.IndexExpression. got=%T", stmt.Expression)
